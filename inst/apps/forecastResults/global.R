9a3b140e91e374a258215b9bdc6858d698490792
jtelliott <jaketelliott@gmail.com>
2018-03-15 09:14:58 -0400

    ugh

--- inst/apps/forecastResults/global.R
+++ inst/apps/forecastResults/global.R
@@ -1,10 +1,28 @@
 library(shiny)
 library(shinythemes)
-library(choroplethr)
-library(choroplethrMaps)
+library(magrittr)
+library(ggplot2)
+library(forecast)
 
-data("df_state_demographics")
-map_data <- df_state_demographics
+
+# load in data set
+data("attrition")
+attrition <- data.frame(attrition)
+
+# declare as  time series object
+df.ts <- ts(attrition, start = c(2004, 10), frequency = 12)
+
+# impute aberrant months
+dec <- subset(df.ts[,'Total'], month = 12)
+dec.ind <- which(cycle(df.ts[,'Total']) == 12)
+nov <- subset(df.ts[,'Total'], month = 11)
+nov.ind <- which(cycle(df.ts[,'Total']) == 11)
+dec[c(3,4,11)] <- mean(dec[-c(3,4,11)])
+nov[c(3,4,11)] <- mean(nov[-c(3,4,11)])
+
+# place imputed values back into the original series.
+df.ts[dec.ind, 'Total'] <- dec
+df.ts[nov.ind, 'Total'] <- nov
 
 
 
--- inst/apps/forecastResults/ui.R
+++ inst/apps/forecastResults/ui.R
@@ -1,27 +1,98 @@
-ui <- fluidPage(title = "My First App!",
-                responsive = NULL,
-                theme = shinythemes::themeSelector(),
+ui <- fluidPage(
+  responsive = NULL,
+  theme = shinythemes::themeSelector(),
+  
+  titlePanel("Forecasting Attrition via Dynamic Regression", windowTitle = "attrition-forecast"),
+
+  sidebarLayout(
     
+    # Inputs
+    sidebarPanel(width = 3,
+                 
+      selectInput(inputId = "iteration",
+                  label = "Iteration",
+                  choices = c()),           
+      
+      #reg ar component
+      sliderInput("ar",
+                  label = "Autoregressive Order",
+                  min = 0,
+                  max = 5,
+                  value = 1),
 
-                sidebarLayout(
-                  sidebarPanel(width = 3,
-                    sliderInput("num_colors",
-                                label = "Number of colors:",
-                                min = 1,
-                                max = 9,
-                                value = 7),
-                    selectInput("select",
-                                label = "Select Demographic:",
-                                choices = colnames(map_data)[2:9],
-                                selected = 1)),
+      #reg diff component
+      sliderInput("i",
+                  label = "Differencing Order",
+                  min = 0,
+                  max = 1,
+                  value = 0),
 
+      #reg ma component
+      sliderInput("ma",
+                  label = "Moving-Average Order",
+                  min = 0,
+                  max = 5,
+                  value = 1),
 
-                  mainPanel(width = 9,
-                      tabsetPanel(
-                        tabPanel(title = "Output Map",
-                                 plotOutput(outputId = "map")),
-                        tabPanel(title = "Data Table",
-                                 dataTableOutput(outputId = "table"))))))
+      #seasonal ar component
+      sliderInput("sar",
+                  label = "Seasonal Autoregressive Order",
+                  min = 0,
+                  max = 5,
+                  value = 1),
+
+      #seasonal diff component
+      sliderInput("si",
+                  label = "Seasonal Differencing Order",
+                  min = 0,
+                  max = 2,
+                  value = 0),
+
+      #seasonal ma component
+      sliderInput("sma",
+                  label = "Seasonal Moving-Average Order",
+                  min = 0,
+                  max = 5,
+                  value = 1),
+
+      #econ var selection
+      checkboxGroupInput(inputId = "econvars",
+                         label = "Economic Indicators",
+                         choices = c(
+                           "Unemployment Adj" = "Unemployment.Rate.Adj",
+                           "Unemployment NonAdj" = "Unemployment.Rate.NonAdj",
+                           "CPI" = "CPI.Adj",
+                           "Nonfarm Jobs Adj" = "Nonfarm.Jobs.Adj",
+                           "Nonfarm Jobs NonAdj"= "Nonfarm.Jobs.NonAdj",
+                           "Labor Force Participation" = "Labor.Force.Participation",
+                           "Labor Market Momentum" = "Labor.Market.Momentum",
+                           "Real GDP per capita" = "Real.GDP.Per.Capita",
+                           "Nonfarm Job Quits" = "Nonfarm.Quits.Adj")),
+      
+      # Built with Shiny by RStudio
+      br(),br(),    # Two line breaks for visual separation
+      h5("Built with",
+         img(src = "https://www.rstudio.com/wp-content/uploads/2014/04/shiny.png", height = "30px"),
+         "by",
+         img(src= "https://www.rstudio.com/wp-content/uploads/2014/07/RStudio-Logo-Blue-Gray.png", height = "30px"),
+         ".")),
+    
+    # Outputs
+    mainPanel(width = 9,
+              
+              tabsetPanel(id = "tabspanel", type = "tabs",
+                          
+                # tabPanel(title = "Introduction",
+                #          uiOutput(outputId = "introduction")),
+                
+                tabPanel(title = "Model Fit",
+                         plotOutput(outputId = "fit")),
+                
+                tabPanel(title = "Model Summary",
+                         dataTableOutput(outputId = "summary")),
+                
+                tabPanel(title = "Residuals",
+                         plotOutput(outputId = "residuals"))))))
 
 
 
